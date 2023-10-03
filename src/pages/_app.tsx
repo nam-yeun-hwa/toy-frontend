9de703e94d6138d8ddd7f5db5e868cdf19224a26
nam-yeun-hwa <nyh2ya@gmail.com>
2023-10-04 01:20:42 +0900

    Feat(_app):axios설치, react-query설치 _app페이지에 리액트쿼리 사용관련 내용추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,41 @@ import GlobalStyle from "@/styles/global";
 import "@/styles/globals.css";
 import globalTheme from "@/styles/globalTheme";
 import { ThemeProvider } from "@emotion/react";
+import {
+  Hydrate,
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 import type { AppProps } from "next/app";
+
+export const queryErrorHandler = (error) => {
+  console.log(error, "app queryErrorHandler");
+
+  // toast(`데이터를 가져오지 못했습니다! ${error.message}`);
+};
+
+const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: (error, query) => {},
+  }),
+  defaultOptions: {
+    queries: {
+      retry: 0,
+    },
+    mutations: {},
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={globalTheme}>
-      <GlobalStyle />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate>
+        <ThemeProvider theme={globalTheme}>
+          <GlobalStyle />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </Hydrate>
+    </QueryClientProvider>
   );
 }
