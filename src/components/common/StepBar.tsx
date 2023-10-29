c0acc7e879a804a44369b035df7f18e8570f509a
nam-yeun-hwa <nyh2ya@gmail.com>
2023-10-29 21:52:50 +0900

    Feat(StepBar): 회원가입페이지에서 사용할 스텝바 페이지 생성

---
+++ src/components/common/StepBar.tsx
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+import React from 'react';
+
+const StepBarBox = styled.div``;
+
+function StepBar(): JSX.Element {
+	return <StepBarBox></StepBarBox>;
+}
+
+export default React.memo(StepBar);
